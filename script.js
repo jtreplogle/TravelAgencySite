6a8bcd3aca2216b00c958c4fab2a36eefcad6b95
New Owner <newowner@Dysons-MBP.att.net>
2016-01-16 17:57:11 -0600

    added objects to js file

--- script.js
+++ script.js
@@ -11,24 +11,28 @@ var destinations = [];
 	};
 destinations.push(singapore);	
 
-		var singapore = {
-		city: "Singapore",
-		imgUrl: "http://phocus.com.sg/uploads/2010/02/MG_0940-Edit-2_s.jpg",
-		description: "The fabulous location, family and child-friendly environment, and range of fun amenities make this vibrant hotel the ideal family getaway. Located at the heart of the action near Festive Walk and World Square, Festive Hotel puts you just minutes away from Universal Studios Singapore®, S.E.A. AquariumTM – the world’s largest aquarium, Adventure Cove WaterparkTM, and all-day dining, shopping and entertainment.",
-		price: "Prices Starting at $1050",
+
+		var hanoi = {
+		city: "Hanio, Vietnam",
+		imgUrl: "http://toureast.vn/files/tours/short-stay/ha-long-bay_360_360.jpg",
+		description: "Accommodation in Vietnam ranges from simple dorm accommodation in backpacking hostels to world-class resorts, both in large cities and in popular coastal and rural destinations. This list presents the best luxury resorts in Vietnam, as rated by visitors, picked from an ever-growing selection of boutique hotel stays, luxury hotel hideaways and romantic retreats.",
+		price: "Prices Starting at $1100",
 		buttonLink: "booknow.html",
 		buttonText: "I want this vacation"		
 	};
-destinations.push(singapore);	
-	var singapore = {
-		city: "Singapore",
+destinations.push(hanoi);	
+
+
+
+	var bangkok = {
+		city: "Bangkok, Thailand",
 		imgUrl: "http://phocus.com.sg/uploads/2010/02/MG_0940-Edit-2_s.jpg",
-		description: "The fabulous location, family and child-friendly environment, and range of fun amenities make this vibrant hotel the ideal family getaway. Located at the heart of the action near Festive Walk and World Square, Festive Hotel puts you just minutes away from Universal Studios Singapore®, S.E.A. AquariumTM – the world’s largest aquarium, Adventure Cove WaterparkTM, and all-day dining, shopping and entertainment.",
-		price: "Prices Starting at $1050",
+		description: "Golden palaces, floating markets, majestic porcelain-laid spires…you've never seen a capital city quite like Bangkok. Visit Pratunam or Siam Square for premium shopping, then unwind in the European-style gardens of Dusit. Thon Buri is home to the awesome Wat Arun temple, and over in Phra Nakhon, you’ll find the Wat Pho temple of the Reclining Buddha. Savor mango sticky rice at a food stall before taking in the gilded splendor of the Grand Palace.",
+		price: "Prices Starting at $1250",
 		buttonLink: "booknow.html",
 		buttonText: "I want this vacation"		
 	};
-destinations.push(singapore);	
+destinations.push(bangkok);	
 
 
 
