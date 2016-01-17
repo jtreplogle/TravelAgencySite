c5237c6f6a7e498254391021ca38c2b1b55f3b12
New Owner <newowner@Dysons-MBP.att.net>
2016-01-17 16:20:43 -0600

    adding Jason's js

--- script.js
+++ script.js
@@ -74,6 +74,39 @@ var destinations = [];
 	};
 	destinations.push(morocco);	//adds to destinations array
 
+	var ireland = {
+		city: "Dublin, Ireland",
+		imgUrl: "http://i1379.photobucket.com/albums/ah123/jreplogle37/Ireland_zps5v1yn274.jpg",
+		description: "Home to sweeping views as seen in Star Wars VII, Greater Dublin has a population of just under two million, yet the bustling city center is compact enough to explore on foot or a rented bicycle. Its rebellious and joyous Celtic traditions are alive in the music spilling out from the pubs, while the historic buildings and monuments tell the stories of the country’s rich religious and cultural heritage. Take part in Bloomsday festivities on June 16 each year when locals celebrate the life of author James Joyce, one of Dublin’s most famous residents.",
+		price: "Prices Starting at $1950",
+		buttonLink: "booknow.html",
+		buttonText: "I want this vacation"		
+	};
+	destinations.push(ierland);	
+
+
+		var newZealand = {
+		city: "Queenstown, New Zealand",
+		imgUrl: "http://i1379.photobucket.com/albums/ah123/jreplogle37/New%20Zeland_zpsnn4s68yt.jpg",
+		description: "New Zealand is a country in the southwestern Pacific Ocean consisting of 2 main islands, both marked by volcanoes and glaciation. Capital Wellington, on the North Island, is home to Te Papa Tongarewa, the expansive national museum. Wellington’s dramatic Mt. Victoria and the South Island’s Fiordland and Southern Lakes stood in for mythical Middle Earth in Peter Jackson’s Lord of the Rings films.",
+		price: "Prices Starting at $3800",
+		buttonLink: "booknow.html",
+		buttonText: "I want this vacation"		
+	};
+	destinations.push(newZealand);	
+
+
+
+	var boraBora = {
+		city: "Bora Bora, Tahiti",
+		imgUrl: "http://i1379.photobucket.com/albums/ah123/jreplogle37/Bora%20Bora_zpsktl2o4jp.jpg",
+		description: "Bora Bora is a small South Pacific island northwest of Tahiti in French Polynesia. Surrounded by sand-fringed motus (islets) and a turquoise lagoon protected by a coral reef, it’s known for its scuba diving. It's also a popular luxury resort destination where some guest bungalows are perched over the water on stilts. At the island's center rises 727m Mt. Otemanu, a dormant volcano.",
+		price: "Prices Starting at $3250",
+		buttonLink: "booknow.html",
+		buttonText: "I want this vacation"		
+	};
+	destinations.push(boraBora);	
+
 // console.log("destinations", destinations);
 
 
